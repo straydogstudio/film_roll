3d6e8ed83a19204dfd94c52a7f3f096a1b20ac21
Noel Peden <noel@peden.biz>
2014-12-15 11:40:22 -0800

    Use grunt for watch/compile/minimize

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    coffee: {
+      compile: {
+        options: {
+          bare: true
+        },
+        files: {
+          'js/jquery.film_roll.js': 'coffee/jquery.film_roll.coffee'
+        }
+      }
+    },
+    uglify: {
+      js: {
+        files: {
+          'js/jquery.film_roll.min.js': 'js/jquery.film_roll.js'
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['coffee/*.coffee'],
+        tasks: ['coffee','uglify:js']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['coffee','uglify:js']);
+};
